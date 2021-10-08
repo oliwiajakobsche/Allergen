39df07877d681e13adb20211e9bfb5706adb360d
OliviaJ <liqhta@gmail.com>
2021-10-08 10:22:40 +0200

    Empty React Native app project created.

---
+++ Allergen.Backend/Contracts/V1/Requests/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllergenBackend.Contracts.V1.Requests
+{
+    public class LoginRequest
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Allergen.Backend/Contracts/V1/Requests/SignupRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllergenBackend.Contracts.V1.Requests
+{
+    public class SignupRequest
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int SexId { get; set; }
+    }
+}
